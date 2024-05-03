08a6ba8873806ad187b8aebf1bcb837603ca7e24
Maarten Mulders <mthmulders@apache.org>
2024-05-03 10:56:38 +0200

    Use $HOMEBREW_PREFIX when present

--- includes/setup.inc.sh
+++ includes/setup.inc.sh
@@ -6,9 +6,10 @@ function __debug() {
 }
 
 if type brew &>/dev/null; then
-  __debug "Detected Homebrew" 
+  hbdir=${HOMEBREW_PREFIX:-/opt/homebrew}
+  __debug "Detected Homebrew in $hbdir" 
   # Find maven-snapshot installation directory
-  mvn4dir=$(find /opt/homebrew/Cellar/maven-snapshot -type d -name "4*")
+  mvn4dir=$(find $hbdir/Cellar/maven-snapshot -type d -name "4*")
   if [ -f $mvn4dir/bin/mvn ] && [ -x $mvn4dir/bin/mvn ]; then
     __debug Found Maven 4 and it is executable: $($mvn4dir/bin/mvn --version | head -n 1)
     function mvn4() {
@@ -17,7 +18,7 @@ if type brew &>/dev/null; then
   fi
 
   # Find maven installation directory
-  mvn3dir=$(find /opt/homebrew/Cellar/maven -type d -name "3*")
+  mvn3dir=$(find $hbdir/Cellar/maven -type d -name "3*")
   if [ -f $mvn3dir/bin/mvn ] && [ -x $mvn3dir/bin/mvn ]; then
     __debug Found Maven 3 and it is executable: $($mvn3dir/bin/mvn --version | head -n 1)
     function mvn3() {
