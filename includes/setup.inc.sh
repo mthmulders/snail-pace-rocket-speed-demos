ce58c625f3c7f47de95ac29c692a841052cb1db1
Maarten Mulders <mthmulders@apache.org>
2024-05-03 11:00:08 +0200

    Surround path with quotes to prevent splitting on spaces

--- includes/setup.inc.sh
+++ includes/setup.inc.sh
@@ -10,7 +10,7 @@ if type brew &>/dev/null; then
   __debug "Detected Homebrew in $hbdir" 
   # Find maven-snapshot installation directory
   mvn4dir=$(find $hbdir/Cellar/maven-snapshot -type d -name "4*")
-  if [ -f $mvn4dir/bin/mvn ] && [ -x $mvn4dir/bin/mvn ]; then
+  if [ -f "$mvn4dir/bin/mvn" ] && [ -x "$mvn4dir/bin/mvn" ]; then
     __debug Found Maven 4 and it is executable: $($mvn4dir/bin/mvn --version | head -n 1)
     function mvn4() {
       $mvn4dir/bin/mvn "${@}"
@@ -19,7 +19,7 @@ if type brew &>/dev/null; then
 
   # Find maven installation directory
   mvn3dir=$(find $hbdir/Cellar/maven -type d -name "3*")
-  if [ -f $mvn3dir/bin/mvn ] && [ -x $mvn3dir/bin/mvn ]; then
+  if [ -f "$mvn3dir/bin/mvn" ] && [ -x "$mvn3dir/bin/mvn" ]; then
     __debug Found Maven 3 and it is executable: $($mvn3dir/bin/mvn --version | head -n 1)
     function mvn3() {
       $mvn3dir/bin/mvn "${@}"
