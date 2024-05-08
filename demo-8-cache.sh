e185003d8608e1568bb0870239b93937239db5d3
Benjamin Marwell <bmarwell@apache.org>
2024-05-08 11:24:34 +0200

    re-enable modification showcase

--- demo-8-cache.sh
+++ demo-8-cache.sh
@@ -12,26 +12,27 @@ pushd shiro
 echo "Press enter for a first run"
 read var
 
-mvnd3 --file pom.xml test --projects '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
+# need to use at least package!
+mvnd3 --file pom.xml package --projects '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
 
 echo ""
 echo "Press enter for a new run"
 echo ""
 read var
 
-mvnd3 --file pom.xml test --projects '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
+mvnd3 --file pom.xml package --projects '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
 
-# echo ""
-# read var
-# 
-# echo ""
-# echo "Now edit the file ./shiro/web/src/main/java/org/apache/shiro/web/servlet/SimpleCookie.java"
-# echo ""
-# perl -i.bak -pe 's,null/empty,null or empty,g' "./web/src/main/java/org/apache/shiro/web/servlet/SimpleCookie.java"
-# rm -f "./web/src/main/java/org/apache/shiro/web/servlet/SimpleCookie.java.bak"
-# echo ""
-# read var
-# 
-# mvn --file pom.xml test --projects '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
+echo ""
+read var
+
+echo ""
+echo "Now edit the file ./shiro/web/src/main/java/org/apache/shiro/web/servlet/SimpleCookie.java"
+echo ""
+perl -i.bak -pe 's,null/empty,null or empty,g' "./web/src/main/java/org/apache/shiro/web/servlet/SimpleCookie.java"
+rm -f "./web/src/main/java/org/apache/shiro/web/servlet/SimpleCookie.java.bak"
+echo ""
+read var
+
+mvnd3 --file pom.xml package --projects '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
 
 popd
