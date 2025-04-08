e1b59dd59c74e2831f18804265994766d7f8e071
Benjamin Marwell <bmarwell@apache.org>
2025-04-08 09:18:49 +0200

    check for Java 21+ on the PATH

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# TODO Somehow ensure we're on Java 21
+if ! command -v java >/dev/null 2>&1 && [ -z "${JAVA_HOME}" ]; then
+  >&2 echo "[ERROR] no java in path. Please set JAVA_HOME and PATH."
+  exit 1
+fi
+
+# if java is not in path, do so now.
+if ! command -v java >/dev/null 2>&1; then
+  PATH="${JAVA_HOME}/bin:${PATH}"
+  export PATH
+fi
+
+# check if Java 21 or newer is being used
+
+java_spec_version="$(java -XshowSettings:vm -XshowSettings:properties  --version 2>&1 | grep "java.specification.version")"
+java_spec_version="$(echo "$java_spec_version" | cut -f 2 -d "=" | tr -d '[:space:]')"
+
+if [ "${java_spec_version}" -lt 21 ]; then
+  >&2 echo "[ERROR] you need java 21, but you are using ${java_spec_version}"
+  exit 1
+fi
 
 # Delete cache
 if [ -e "$HOME/.m2/build-cache/v1/org.apache.shiro" ]; then
@@ -18,4 +37,12 @@ git submodule update --recursive
 # Do a one-time build so that we have (most) dependencies and plugins downloaded
 pushd shiro
 mvn verify -pl '!org.apache.shiro.integrationtests:shiro-its-jakarta-ee'
+popd
+
+pushd demo-resources/app-simple
+mvnd verify -q -ntp
+popd
+
+pushd demo-resources/app-more-modules
+mvnd verify -q -ntp
 popd
