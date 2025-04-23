ff884302ae226df4d2f9626531e1dd59c7873555
Benjamin Marwell <bmarwell@apache.org>
2025-04-23 12:52:33 +0200

    more StreamHelper tests

--- demo-resources/app-simple/common/src/main/java/de/bmarwell/snailspace/demo4/app/common/value/StreamHelper.java
+++ demo-resources/app-simple/common/src/main/java/de/bmarwell/snailspace/demo4/app/common/value/StreamHelper.java
@@ -1,6 +1,26 @@
 package de.bmarwell.snailspace.demo4.app.common.value;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class StreamHelper {
+
+    public static void copyTo(
+        InputStream inputStream, OutputStream outputStream) throws IOException {
+        try {
+            Thread.sleep(Long.parseLong(System.getProperty("method.timeout")));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] buffer = new byte[512];
+        int read;
+
+        while ((read = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, read);
+        }
+    }
 
     @Override
     public int hashCode() {
--- demo-resources/app-simple/common/src/test/java/de/bmarwell/snailspace/demo4/app/common/value/StreamHelperTest.java
+++ demo-resources/app-simple/common/src/test/java/de/bmarwell/snailspace/demo4/app/common/value/StreamHelperTest.java
@@ -1,5 +1,11 @@
 package de.bmarwell.snailspace.demo4.app.common.value;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +17,21 @@ class StreamHelperTest {
 
         // then
         Assertions.assertThat(sh).isEqualTo(new StreamHelper());
+    }
+
+    @Test
+    void test_copy_to() throws IOException {
+        // given
+        final var in = new ByteArrayInputStream("test".getBytes(StandardCharsets.UTF_8));
+        final var out = new ByteArrayOutputStream();
+
+        // when
+        StreamHelper.copyTo(in, out);
+
+        // then
+        assertThat(out.toByteArray()).isNotNull();
+        final String output = out.toString(StandardCharsets.UTF_8);
+        assertThat(output).isEqualTo("test");
     }
 
 }
