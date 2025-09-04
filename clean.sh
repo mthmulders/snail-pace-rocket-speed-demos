327986601e1340467a83369634bfa33ecccd8dc7
Benjamin Marwell <bmarwell@apache.org>
2025-09-04 07:35:30 +0200

    fix clean.sh to clean new demo project

--- clean.sh
+++ clean.sh
@@ -2,12 +2,10 @@
 set -uo pipefail
 # no set -x; if a file or container isn't there, it's OK
 
-rm shiro/.mvn/extensions.xml
-rm shiro/crypto/core/src/test/groovy/org/apache/shiro/crypto/FailingTest.java
+rm demo-resources/.mvn/extensions.xml 2>/dev/null || true
+find demo-resources/ -name "junit-platform.properties" -exec rm '{}' +
 
-find shiro/ -name "junit-platform.properties" -exec rm '{}' +
+(cd demo-resources && git clean -fxd && git reset --hard HEAD)
 
-(cd shiro && git clean -fxd && git reset --hard HEAD)
-
-rm -Rf ~/.m2/build-cache/v1/org.apache.shiro*
-rm -Rf ~/.m2/build-cache/v1.1/org.apache.shiro*
+rm -Rf ~/.m2/build-cache/v1/de.bmarwell.snailspace.demo4*
+rm -Rf ~/.m2/build-cache/v1.1/de.bmarwell.snailspace.demo4*
