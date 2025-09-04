4619acd1a8118d44c43183a76c271985333d848a
Benjamin Marwell <bmarwell@apache.org>
2025-09-04 09:33:53 +0200

    add a notice what we are running at demo 2

--- demo-2-parallel-tests.sh
+++ demo-2-parallel-tests.sh
@@ -7,6 +7,13 @@ set -euox pipefail
 
 pushd demo-resources/app-simple
 mvn3 clean --quiet
+
+echo
+echo
+echo "now running mvn verify with parallel tests enabled"
+echo
+echo
+
 read
 
 mvn3 verify -Dotel.traces.exporter=otlp --activate-profiles parallel-tests
